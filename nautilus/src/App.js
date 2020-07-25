cbbb170f14cb5872dd7c2b9aa260e029dd67bee3
Omari Brown <omaris.brown@gmail.com>
2020-07-25 13:28:31 -0400

    Created react app

---
+++ nautilus/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='landing-div'>
+        This is the landing div.
+      </div>
+      <div className='paralax-div'>
+        <div class='window'>
+
+        </div>
+      </div>
+      <div className='content-div'></div>
+    </div>
+  );
+}
+
+export default App;
