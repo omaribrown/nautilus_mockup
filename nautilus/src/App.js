13c7e289de42c54fe23d2aa861f12b525d5f9395
Omari Brown <omaris.brown@gmail.com>
2020-07-27 14:21:52 -0400

    swithing to navbar with issues

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
-import LandingParalax from './components/LandingParalax'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <LandingParalax />
+       <NavBar />
     </div>
   );
 }
--- nautilus/src/components/LandingParalax.jsx
+++ nautilus/src/components/LandingParalax.jsx
@@ -6,14 +6,28 @@ export default class LandingParalax extends React.Component {
     render() {
         return (
             <div>
-                <div className='landing-div'>
-                    LANDING
+                {/* <div className="landing-div">
+                    this is the top div
                 </div>
+                <div className="para">
+                    <div className="paraInner">
+                        <div className="window">
+                            <h1>fixed text</h1>
+                        </div>
+                    </div>
+                </div> */}
+
                 <div className='content-div'>
                     <div className='content-inner'>
                         stuff
                     </div>
                 </div>
+                <div className='landing-div'>
+                    LANDING
+                </div>
+                {/* <div className="bottom-div">
+                    This is the bottom div
+                </div> */}
             </div>
         )
     }
