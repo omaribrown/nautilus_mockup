a74d74ea6cfd25dbd9d5c3a0271e5fc64ababcdb
Omari Brown <omaris.brown@gmail.com>
2020-07-26 21:28:53 -0400

    css updates

--- nautilus/src/components/LandingParalax.jsx
+++ nautilus/src/components/LandingParalax.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import './Styles/LandingParalax.css'
+import arrow from './assets/arrow2.png'
 
 export default class LandingParalax extends React.Component {
     render() {
         return (
             <div>
-                <div class="landing-div">
-                    
+                <div className="landing-div">
+                    <div className='landing-content'>
+                        <h4>Issue 079</h4>
+                        <h1>Catalyst</h1>
+                        <p>Agents of change</p>
+                        <a href='#'><strong>Read More</strong></a>
+                        <br />
+                        <button>SEE FULL ISSUE</button>
+                    </div>
+                    <div className='landing-scroll'>
+                        <img src={arrow} />
+                        <br />
+                        <a href='#'>SCROLL DOWN</a>
+                    </div>
                 </div>
-                <div class="para">
-                    <div class="paraInner">
-                        <div class="window">
-                            
+                <div className="homepage-content">
+                    <div className="paraInner">
+                        <div className="window">
+                            <h1></h1>
                         </div>
                     </div>
                 </div>
-                <div class="bottom-div">
+                {/* <div className="bottom-div">
                     This is the bottom div
-                </div>
+                </div> */}
             </div>
         )
     }
