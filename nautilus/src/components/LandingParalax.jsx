efa239a03330fa386bf6ab8e5d3d8b2e1823050f
Omari Brown <omaris.brown@gmail.com>
2020-07-26 22:03:36 -0400

    FINALLY FIGURED OUT PARALLAX

--- nautilus/src/components/LandingParalax.jsx
+++ nautilus/src/components/LandingParalax.jsx
@@ -6,26 +6,12 @@ export default class LandingParalax extends React.Component {
     render() {
         return (
             <div>
-                {/* <div className="landing-div">
-                    this is the top div
-                </div>
-                <div className="para">
-                    <div className="paraInner">
-                        <div className="window">
-                            <h1>fixed text</h1>
-                        </div>
-                    </div>
-                </div> */}
-
-                <div className='content-div'>
-                    CONTENT
-                </div>
                 <div className='landing-div'>
                     LANDING
                 </div>
-                {/* <div className="bottom-div">
-                    This is the bottom div
-                </div> */}
+                <div className='content-div'>
+                    CONTENT
+                </div>
             </div>
         )
     }
